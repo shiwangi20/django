3fa8cf547634d025673a51211150aca0e0d9dba7
shiwangi20 <gargshiwangi2703@gmail.com>
2019-10-12 17:11:17 +0530

    Http404

--- music/views.py
+++ music/views.py
@@ -1,6 +1,7 @@
-from django.http import HttpResponse
 from .models import Album
 from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import get_object_or_404
 
 
 def index(request):
@@ -12,4 +13,5 @@ def index(request):
 
 
 def details(request, album_id):
-    return HttpResponse('Details of album %s.' % album_id)
+    album = get_object_or_404(Album, pk=album_id)
+    return render(request, 'music/details.html', {'album': album})
