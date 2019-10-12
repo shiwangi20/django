38fb768ee6b4b42865c03e71d9743f81c3610752
shiwangi20 <gargshiwangi2703@gmail.com>
2019-10-12 13:35:54 +0530

    details view with argument

--- music/views.py
+++ music/views.py
@@ -6,3 +6,7 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse('This is a Music app homepage')
+
+
+def details(request, album_id):
+    return HttpResponse('Details of album %s.' % album_id)
