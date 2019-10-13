8f7318b35da723ef785c652fd3eb7c0c73d8e4c2
shiwangi20 <gargshiwangi2703@gmail.com>
2019-10-13 17:41:33 +0530

    form

--- music/urls.py
+++ music/urls.py
@@ -6,6 +6,6 @@ app_name = 'music'
 
 urlpatterns = [
     path('', views.index, name='index'),
-
     path('<int:album_id>/', views.details, name='details'),
+    path('<int:album_id>/favorite/', views.favorite, name='favorite'),
 ]
--- music/views.py
+++ music/views.py
@@ -1,7 +1,7 @@
-from .models import Album
-from django.shortcuts import render
-from django.http import Http404
 from django.shortcuts import get_object_or_404
+from django.shortcuts import render
+
+from .models import Album, Song
 
 
 def index(request):
@@ -15,3 +15,18 @@ def index(request):
 def details(request, album_id):
     album = get_object_or_404(Album, pk=album_id)
     return render(request, 'music/details.html', {'album': album})
+
+
+def favorite(request, album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    try:
+        selected_song = album.song_set.get(pk=request.POST['song'])
+    except (KeyError, Song.DoesNotExist):
+        return render(request, 'music/details.html',
+                      {'album': album,
+                       'error_message': "You did not select anything",
+                       })
+    else:
+        selected_song.is_favorite = True
+        selected_song.save()
+        return render(request, 'music/details.html', {'album': album})
