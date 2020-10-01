9b94e798bb5a730b413c90fa8094080b713e73b9
pankaj2319975 <60501879+pankaj2319975@users.noreply.github.com>
2020-10-01 21:35:24 +0530

    Update admin.py

--- music/admin.py
+++ music/admin.py
@@ -1,8 +1,4 @@
 from django.contrib import admin
 from .models import Album, Song
 
-admin.site.register(Album)
-admin.site.register(Song)
-
-#line no 4&5 best alternative for multiple model register
 admin.register(Album, Song)(admin.ModelAdmin)
